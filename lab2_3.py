43e12922f2acfdba2722aaf7d3f5f5b3109a6fb2
Bhumi Shah <bhumishah@Bhumis-MacBook-Pro.local>
2026-03-18 19:30:29 +0530

    third commit

--- lab2_3.py
+++ lab2_3.py
@@ -1,46 +1,46 @@
-# #Question 1
-# number = ''
-# while(number != 0):
-#     number = int(input("please enter any number: "))
-#     print(f"you entered {number}")
+#Question 1
+number = ''
+while(number != 0):
+    number = int(input("please enter any number: "))
+    print(f"you entered {number}")
 
 
-# #Question 2
-# print("square of the number")
-# for i in range(1,11):
-#     print(i*i)
+#Question 2
+print("square of the number")
+for i in range(1,11):
+    print(i*i)
 
 #Question 3
 number = 1
 while number <= 50:
     if(number%2==0):
-        print(f"{number} is even number")
+        print(number)
     number = number + 1
     
-# #Question 4
-# print("odd numbers only")
-# for i in range(1,21,2):
-#     print(i)
+#Question 4
+print("odd numbers only")
+for i in range(1,21,2):
+    print(i)
 
-# #Question 5
-# print("multiple of 5")
-# for i in range(5,51,5):
-#     print(i)
+#Question 5
+print("multiple of 5")
+for i in range(5,51,5):
+    print(i)
 
-# #Question 6
-# print("reverse the count")
-# for i in range(10,0,-1):
-#     print(i)
+#Question 6
+print("reverse the count")
+for i in range(10,0,-1):
+    print(i)
 
-# #Question 7
-# for i in range(1,51):
-#     if(i%6==0):
-#         print(f"{i} is Divisible by 2 and 3 both")
-#     elif(i%3== 0):
-#         print(f"{i} is Divisible by 3")
-#     elif(i%2==0):
-#         print(f"{i} is Divisible by 2")
-#     else:
-#         print(f"{i} is not divisible by 2, 3 or both")
+#Question 7
+for i in range(1,51):
+    if(i%6==0):
+        print(f"{i} is Divisible by 2 and 3 both")
+    elif(i%3== 0):
+        print(f"{i} is Divisible by 3")
+    elif(i%2==0):
+        print(f"{i} is Divisible by 2")
+    else:
+        print(f"{i} is not divisible by 2, 3 or both")
            
         
